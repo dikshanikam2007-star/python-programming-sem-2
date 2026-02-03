74c055f710d92b8d69a0f4b64a56a4f78287d8dc
dikshanikam2007-star <dikshanikam2007@gmail.com>
2026-02-03 11:18:03 +0530

    Create Exp1 Activity.py

---
+++ Exp1 /Exp1 Activity.py
@@ -0,0 +1,15 @@
+num1= int (input("Enter First Number:"))
+num2= int(input("Enter Second Number:"))
+
+
+sum=num1+num2
+print("Addition=",sum)
+
+sub=num1-num2
+print("Substraction=",sub)
+
+mult=num1*num2
+print("Multiplication=",mult)
+
+div=num1%num2
+print("Division=",div)
