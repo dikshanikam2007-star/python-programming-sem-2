15643cbd07cc1f320416a57ecb3603713a40446b
dikshanikam2007-star <dikshanikam2007@gmail.com>
2026-02-09 15:30:43 +0530

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  9 09:47:11 2026
+
+@author: User
+"""
+
+num1=int(input("enter Number1:"))
+num2=int(input("enter number2:"))
+
+sum=num1+num2
+print("addition=",sum)
+
+sum=num1-num2
+print("substraction=",sum)
+
+sum=num1*num2
+print("multiplication=",sum)
+
+sum=num1/num2
+print("division=",sum)
+
+
+
