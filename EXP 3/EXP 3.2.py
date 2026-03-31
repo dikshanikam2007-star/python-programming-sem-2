b22b043658745194d32e74530650300e18f74b25
dikshanikam2007-star <dikshanikam2007@gmail.com>
2026-03-31 20:12:09 +0530

    Update EXP 3.2.py

--- EXP 3/EXP 3.2.py
+++ EXP 3/EXP 3.2.py
@@ -1,3 +1,9 @@
+#simple interest using a function with parameters for principal,rate,and time
+"""
+Created on Mon Mar 16 15:08:34 2026
+@author: DIKSHA
+"""
+
 def simple_interest(principle,rate,time):
   si=(principle*rate*time)/100
   return si
@@ -7,8 +13,3 @@ t=float(input("Enter time(in years):"))
 
 interest=simple_interest(p,r,t)
 print("Simple Interest is:",interest)
-
-
-
-
-
