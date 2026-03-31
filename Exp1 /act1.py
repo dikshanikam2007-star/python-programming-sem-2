6365366bb03c378c99ef0f59562bd5d82be2fba6
dikshanikam2007-star <dikshanikam2007@gmail.com>
2026-03-31 19:36:52 +0530

    Create act1.py

---
+++ Exp1 /act1.py
@@ -0,0 +1,13 @@
+#Write a Python program to display your name, address, and contact number.
+"""
+Created on Tue Mar 31 19:27:01 2026
+
+@author: DIKSHA
+"""
+name="Diksha Shrikant Nikam"
+address="Urul Patan"
+contact_number="9370883592"
+
+print(f"Name:{name}")
+print(f"Address:{address}")
+print(f"Contact_Number:{contact_number}")
