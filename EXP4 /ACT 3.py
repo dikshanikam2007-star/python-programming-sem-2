8b72a38b2abb93cf4691e5a409b51ec5f3613963
dikshanikam2007-star <dikshanikam2007@gmail.com>
2026-03-31 20:22:04 +0530

    Update ACT 3.py

--- EXP4 /ACT 3.py
+++ EXP4 /ACT 3.py
@@ -1,17 +1,11 @@
 # Reverse a customer feedback message.
 """
 Created on Mon Mar 16 15:51:24 2026
-
 @author: DIKSHA
 """
 
-def reverse_feedback(message):
-    # This reads the string from end to start with a step of -1
-    return message[::-1]
+feedback = "Excellent service and fast delivery!"
+reversed_feedback = feedback[::-1]
 
-# --- Sample Program ---
-customer_msg = "The service was excellent!"
-reversed_msg = reverse_feedback(customer_msg)
-
-print(f"Original: {customer_msg}")
-print(f"Reversed: {reversed_msg}")
+print(f"Original: {feedback}")
+print(f"Reversed: {reversed_feedback}")
